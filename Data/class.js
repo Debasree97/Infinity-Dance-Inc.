ba4d9c975a0981101b70169d6df30a8c1441f042
unknown <atd21821@gmail.com>
2021-10-04 01:24:07 +0600

    first commit

---
+++ Data/class.js
@@ -0,0 +1,90 @@
+const classes = [
+  {
+    id: 1,
+    title: "Dino Dance Party",
+    category: "Toddler",
+    instructor: "Isabelle Wood",
+    price: 60,
+    program: 4,
+    descrption:
+      "Stomp and Roar like a Dinosaur! Little dancers will have a blast dancing and learning about Dinosaurs in this 4 week program.",
+    img: "https://images.squarespace-cdn.com/content/v1/5e18ece17e30bc7bf7ace053/1619836135685-U5ANAUESFKAC27VOD2HY/DINOSAUR%2BDANCE%2BPARTY.png?format=500w",
+  },
+  {
+    id: 2,
+    title: "Royal Dance Academy",
+    category: "Toddler",
+    instructor: "Clary Fray",
+    price: 60,
+    program: 4,
+    descrption:
+      "Magic wands, royal crowns and shiny jewels! Your dancer is guaranteed to have a fancy time dancing in our 4 week Royal Ball!",
+    img: "https://images.squarespace-cdn.com/content/v1/5e18ece17e30bc7bf7ace053/1619836241064-N71XIT4RM0DUGZRVUNIG/THE%2BROYAL%2BDANCE%2BACADEMY.png?format=500w",
+  },
+  {
+    id: 3,
+    title: "Beach Blast",
+    category: "Kid",
+    instructor: "Maia Roberts",
+    price: 60,
+    program: 4,
+    descrption:
+      "Summer fun in the sun. Dancers will learn to dance with joy while getting the wiggle out. This outdoor class is kid-friendly.",
+    img: "https://images.squarespace-cdn.com/content/v1/5e18ece17e30bc7bf7ace053/1619554508465-DUYOCC5O5D24T0NFURZM/Gorham+Summer+Camps+%2810%29.png?format=500w",
+  },
+  {
+    id: 4,
+    title: "Hip Hopping",
+    category: "Kid",
+    instructor: "Lily Chen",
+    price: 60,
+    program: 4,
+    descrption:
+      "Pop the hip and hop the leap! In our 4 weeks fun program our dancers will become familiar with hip hop moves while having fun",
+    img: "https://images.squarespace-cdn.com/content/v1/5e18ece17e30bc7bf7ace053/1619555992192-OYZN51SM9UN4FUESVD4K/20.png?format=500w",
+  },
+  {
+    id: 5,
+    title: "Basic Hip Hop",
+    category: "Pre-teen",
+    instructor: "Simon Lewis",
+    price: 80,
+    program: 6,
+    descrption:
+      "In Basic Hip Hop class dancers will learn beginners hip hop moves. One complete routine is included at the end of the program.",
+    img: "https://images.squarespace-cdn.com/content/v1/5e18ece17e30bc7bf7ace053/1619555992192-OYZN51SM9UN4FUESVD4K/20.png?format=500w",
+  },
+  {
+    id: 6,
+    title: "Basic Contemporary",
+    category: "Pre-teen",
+    instructor: "Magnus Bane",
+    price: 80,
+    program: 6,
+    descrption:
+      "In Basic Contemporary class dancer will learn basic contemporary moves. One complete routine is also included in this program.",
+    img: "https://images.squarespace-cdn.com/content/v1/5e18ece17e30bc7bf7ace053/1620225099607-MKGIGP1BX9O2NDO4Y99L/NEW+contemp.png?format=500w",
+  },
+  {
+    id: 7,
+    title: "Intermediate Hip Hop",
+    category: "Teen",
+    instructor: "Maryse Gray",
+    price: 95,
+    program: 7,
+    descrption:
+      "In Intermediate Hip Hop class dancers will learn intermediate hip hop moves. One complete routine is included in this program.",
+    img: "https://images.squarespace-cdn.com/content/v1/5e18ece17e30bc7bf7ace053/1619555992192-OYZN51SM9UN4FUESVD4K/20.png?format=500w",
+  },
+  {
+    id: 8,
+    title: "Intermediate Contemporary",
+    category: "Teen",
+    instructor: "Magnus Bane",
+    price: 95,
+    program: 7,
+    descrption:
+      "In Intermediate Contemporary class dancers will learn intermediate contemporary moves. One complete routine is also included.",
+    img: "https://images.squarespace-cdn.com/content/v1/5e18ece17e30bc7bf7ace053/1620225099607-MKGIGP1BX9O2NDO4Y99L/NEW+contemp.png?format=500w",
+  },
+];
--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import About from "./components/About/About";
+import Classes from "./components/Classes/Classes";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/classes">
+            <Classes></Classes>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/"></Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
