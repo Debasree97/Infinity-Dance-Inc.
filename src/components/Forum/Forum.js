82387bffe93a836d99e7f1d67463ba2908076a50
unknown <atd21821@gmail.com>
2021-10-05 14:48:23 +0600

    final retouch

--- src/components/Forum/Forum.js
+++ src/components/Forum/Forum.js
@@ -21,11 +21,14 @@ const Forum = () => {
         alignItems: "center",
         height: "100%",
         pt: 20,
-        pb: 40,
+        pb: 32,
       }}
       style={bgStyle}
     >
       <Box sx={{ textAlign: "center", p: 10 }} style={formBgStyle}>
+        <Typography variant="h5" sx={{ fontWeight: "bold", mb: 2 }}>
+          Wanna keep track of our classes?
+        </Typography>
         <Typography variant="h2" sx={{ fontWeight: "bold", mb: 3 }}>
           SIGN UP
         </Typography>
@@ -40,36 +43,38 @@ const Forum = () => {
             SIGN IN
           </NavLink>
         </Box>
-        <Button
-          variant="contained"
-          size="large"
-          sx={{ display: "block", mb: 2, width: 300 }}
-          href="https://www.facebook.com/"
-          target="_blank"
-          style={{ backgroundColor: "#385898" }}
-        >
-          Sign Up with Facebook
-        </Button>
-        <Button
-          variant="contained"
-          size="large"
-          sx={{ display: "block", mb: 2, width: 300 }}
-          href="https://www.google.com/"
-          target="_blank"
-          style={{ backgroundColor: "#0057e7" }}
-        >
-          Sign Up with Google
-        </Button>
-        <Button
-          variant="contained"
-          size="large"
-          sx={{ display: "block", mb: 2, width: 300 }}
-          href="https://mail.google.com/mail/u/0/"
-          target="_blank"
-          style={{ backgroundColor: "#32a350" }}
-        >
-          Sign Up with Gmail
-        </Button>
+        <Box sx={{display:"flex",flexDirection:"column",alignItems:"center"}}>
+          <Button
+            variant="contained"
+            size="large"
+            sx={{ display: "block", mb: 2, width: 300 }}
+            href="https://www.facebook.com/"
+            target="_blank"
+            style={{ backgroundColor: "#385898" }}
+          >
+            Sign Up with Facebook
+          </Button>
+          <Button
+            variant="contained"
+            size="large"
+            sx={{ display: "block", mb: 2, width: 300 }}
+            href="https://www.google.com/"
+            target="_blank"
+            style={{ backgroundColor: "#0057e7" }}
+          >
+            Sign Up with Google
+          </Button>
+          <Button
+            variant="contained"
+            size="large"
+            sx={{ display: "block", mb: 2, width: 300 }}
+            href="https://mail.google.com/mail/u/0/"
+            target="_blank"
+            style={{ backgroundColor: "#32a350" }}
+          >
+            Sign Up with Gmail
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
